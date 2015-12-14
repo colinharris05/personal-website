f585a662f1bf397049f5f3d1a445bdf590b90f17
Colin Harris <Colin.Harris@software.dell.com>
2015-12-14 13:14:13 -0600

    Libraries; remove comments; Size of dates next to projects

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-/*jslint browser: true*/
-/*global $, jQuery, angular */
-
-
-//var app = angular.module('app', ['ui.router']);
-//
-//app.controller('mainController', ['$scope', function($scope) {
-//  $scope.name = 'Colin Harris';
-//
-//}]);
-
-// app.js
-
 var app = angular.module('app', ['ui.router']);
 
 app.controller('homeController', ['$rootScope', '$scope', function ($rootScope, $scope) {
