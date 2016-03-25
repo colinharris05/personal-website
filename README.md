1c3f2796c6f1ff0419712a996758f820582b3ff8
= <colinharris05@gmail.com>
2016-03-24 23:12:45 -0500

    NDSC, new courses

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PersonalWebsite
+# Colin Harris
+Personal website written with with HTML5, CSS3, and AngularJS.
--- js/app.js
+++ js/app.js
@@ -3,5 +3,4 @@ var app = angular.module('app', ['ui.router']);
 app.controller('homeController', ['$rootScope', '$scope', function ($rootScope, $scope) {
   "use strict";
 	
-	
 }]);
