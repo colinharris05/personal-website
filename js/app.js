b1990556fa1d815a0bb9e48dc64d1b866227edc5
= <colinharris05@gmail.com>
2016-04-13 00:39:14 -0500

    CSS, styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 var app = angular.module('app', ['ui.router', 'ngMaterial']);
 
-app.controller('homeController', ['$rootScope', '$scope', function ($rootScope, $scope) {
+app.controller('homeController', ['$rootScope', '$scope', '$mdDialog', function ($rootScope, $scope, $mdDialog) {
   "use strict";
-	
+
+//  $scope.showAlert = function(ev) {
+//    // Appending dialog to document.body to cover sidenav in docs app
+//    // Modal dialogs should fully cover application
+//    // to prevent interaction outside of dialog
+//    $mdDialog.show(
+//      $mdDialog.alert()
+//        .parent(angular.element(document.querySelector('#popupContainer')))
+//        .clickOutsideToClose(true)
+//        .title('This is an alert title')
+//        .textContent('You can specify some description text in here.')
+//        .ariaLabel('Alert Dialog Demo')
+//        .ok('Got it!')
+//        .targetEvent(ev)
+//    );
+//  };
+
 }]);
