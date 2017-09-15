dd51585146bcf2cd722bd18469a2b39e3eed94a7
Colin Harris <colinharris05@gmail.com>
2017-09-14 23:06:31 -0700

    Updates

--- js/appRoutes.js
+++ js/appRoutes.js
@@ -1,30 +1,28 @@
 app.config(function($stateProvider, $urlRouterProvider) {
-    
     $urlRouterProvider.otherwise('/home');
-    
     $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'partials/home.html'
-        })
-        .state('aboutMe', {
-          url: '/aboutMe',
-          templateUrl: 'partials/aboutMe.html'
-        })
-        .state('education', {
-          url: '/education',
-          templateUrl: 'partials/education.html'
-        })
-        .state('experience', {
-          url: '/experience',
-          templateUrl: 'partials/experience.html'
-        })
-        .state('projects', {
-          url: '/projects',
-          templateUrl: 'partials/projects.html'
-        })
-        .state('contact', {
-          url: '/contact',
-          templateUrl: 'partials/contact.html'
-        })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'partials/home.html'
+      })
+      .state('aboutMe', {
+        url: '/aboutMe',
+        templateUrl: 'partials/aboutMe.html'
+      })
+      .state('education', {
+        url: '/education',
+        templateUrl: 'partials/education.html'
+      })
+      .state('experience', {
+        url: '/experience',
+        templateUrl: 'partials/experience.html'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'partials/projects.html'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'partials/contact.html'
+      })
 });
